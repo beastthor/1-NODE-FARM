bfa81674435550f16bfde03eec50f9b674beb0a0
kishan_ykc <kishan.ykc@gmail.com>
2023-05-10 22:21:24 -0400

    syncasync

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 
+// Blocking sychronous way
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
+const textOut = `This is what we know about the avocado :${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
+
+// Non-blocking Asynchrnous way 
+fs.readFile('./txt/start.txt','utf-8', (err, data) => { 
+    console.log(data);
+});
+console.log('will read file');
+
